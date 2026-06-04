a03a189ecca2b2114071ebbab747157ca9282d75
Francesco Cassese <francescocassese9@gmail.com>
2026-06-04 16:32:15 +0200

    feat: aggiunta soluzione alla Milestone 3: "Selezionare tutti gli studenti che hanno più di 30 anni"

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -9,3 +9,9 @@ MILESTONE 2 : Selezionare tutti i corsi che valgono più di 10 crediti (479)
 SELECT *
 FROM `courses`
 WHERE `courses`.`cfu` > 10;
+
+MILESTONE 3 : Selezionare tutti gli studenti che hanno più di 30 anni
+
+SELECT *
+FROM `students`
+WHERE `students`.`date_of_birth` > 1996;
