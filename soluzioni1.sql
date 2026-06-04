ffe3e8f9f02427e3e1c6725613408ec391086147
Francesco Cassese <francescocassese9@gmail.com>
2026-06-04 16:22:08 +0200

    feat: aggiunta soluzione alla Milestone 2: " Selezionare tutti i corsi che valgono più di 10 crediti (479)"

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -2,4 +2,10 @@ MILESTONE 1 : Selezionare tutti gli studenti nati nel 1990 (160).
 
 SELECT * 
 FROM `students`
-WHERE YEAR(`students`.`date_of_birth`) = 1990
+WHERE YEAR(`students`.`date_of_birth`) = 1990;
+
+MILESTONE 2 : Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
+SELECT *
+FROM `courses`
+WHERE `courses`.`cfu` > 10;
