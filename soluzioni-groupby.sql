bd624937743be219f208f2afe24df6509b64fac4
Francesco Cassese <francescocassese9@gmail.com>
2026-06-05 17:27:01 +0200

    feat: aggiunta soluzione alla Milestone 2 di GROUP BY: "Contare gli insegnanti che hanno l'ufficio nello stesso edificio)"

--- soluzioni-groupby.sql
+++ soluzioni-groupby.sql
@@ -5,3 +5,10 @@ FROM `students` AS `s`
 WHERE 1
 GROUP BY `enrollment_year`
 ORDER BY `enrollment_year`; 
+
+-- MILESTONE 2: Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT `t`.`office_address`, COUNT(*) AS `teacher_offices`
+FROM `teachers` AS `t`
+WHERE 1
+GROUP BY `t`.`office_address`;
