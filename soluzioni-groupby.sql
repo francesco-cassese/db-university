0f820ba93cf469182dd7595a10ec7070f913a6bf
Francesco Cassese <francescocassese9@gmail.com>
2026-06-05 17:33:48 +0200

    feat: aggiunta soluzione alla Milestone 3 di GROUP BY: "Calcolare la media dei voti di ogni appello d'esame"

--- soluzioni-groupby.sql
+++ soluzioni-groupby.sql
@@ -12,3 +12,10 @@ SELECT `t`.`office_address`, COUNT(*) AS `teacher_offices`
 FROM `teachers` AS `t`
 WHERE 1
 GROUP BY `t`.`office_address`;
+
+-- MILESTONE 3: Calcolare la media dei voti di ogni appello d'esame
+
+SELECT `es`.`exam_id` AS `exam` , AVG(`es`.`vote`) AS `media`
+FROM `exam_student` AS `es`
+WHERE 1
+GROUP BY `exam`
